4fb0b4ad1d67fa486f0e95e01ba83582158f8fac
aksenaksen <ans109905@naver.com>
2025-07-02 18:05:24 +0900

    redis저장 조회 및 10개단위로 MySQL쪽에 백업 구현

---
+++ service/view/src/main/java/mini/board/view/application/ArticleViewService.java
@@ -0,0 +1,43 @@
+package mini.board.view.application;
+
+import lombok.RequiredArgsConstructor;
+import mini.board.view.infra.ArticleViewCountRepository;
+import mini.board.view.infra.ArticleViewDistributedLockRepository;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleViewService {
+    private final ArticleViewCountRepository articleViewCountRepository;
+    private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
+    private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
+    private final StringRedisTemplate redisTemplate;
+    
+
+    private static final int BACK_UP_BACH_SIZE = 100;
+    private static final Duration TTL = Duration.ofMinutes(10);
+
+    public Long increase(Long articleId, Long userId) {
+
+        Set<String> keys = redisTemplate.keys("*");
+        System.out.println("[Redis] 현재 키 목록 = " + keys);
+        if (!articleViewDistributedLockRepository.lock(articleId, userId, TTL)) {
+            return articleViewCountRepository.read(articleId);
+        }
+
+        Long count = articleViewCountRepository.increase(articleId);
+        if (count % BACK_UP_BACH_SIZE == 0) {
+            articleViewCountBackUpProcessor.backUp(articleId, count);
+        }
+        return count;
+    }
+
+    public Long count(Long articleId) {
+        return articleViewCountRepository.read(articleId);
+    }
+}
+
